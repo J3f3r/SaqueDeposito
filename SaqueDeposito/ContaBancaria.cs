6d0f2478d40c913eca9c95278bffb78b88f924a3
J3f3r <jsmartins959@gmail.com>
2023-09-29 03:35:45 -0300

    Exercícios Acadêmicos

---
+++ SaqueDeposito/ContaBancaria.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace SaqueDeposito
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }// numero pode ser acessado(get) mas não alterado (set)
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }// mesmo caso do numero
+
+        public ContaBancaria(int numero, string titular)// construtor com dois argumentos, vc poderia inicializar saldo com zero porem ja vem com zero por padrao
+        {
+            Numero = numero;   //cria conta bancaria sem saldo inicial 
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)// esta aproveitando os dados do construtor acima
+        {
+            Saldo = saldo;   //cria conta com saldo inicial 
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;// descontando a taxa de saque
+        }
+
+        public override string ToString()
+        {
+            return "Conta" 
+                  + Numero  
+                  + "; Titular: " 
+                  + Titular 
+                  + "; Saldo: $" 
+                  + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+
+
+
+}
---
+++ SaqueDeposito/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace SaqueDeposito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;// instanciando conta do tipo ContaBancaria para uso global
+            
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com nome do titular: ");
+            string titular = Console.ReadLine();
+            Console.Write("Havera deposito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre com o valor inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);//deposito inicial corresponde saldo 
+                conta = new ContaBancaria(numero, titular, depositoInicial);//caso tenha um deposito inicial saldo recebera este deposito
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);//jogara zero para o saldo
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);// vai imprimir os dados da conta
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor do deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);// poderia ser a palavra deposito mas como se trata da quantia fica mais claro
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor do saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);// sem o tipo porque ja foi declarada acima
+            conta.Saque(quantia);
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
